49a741612caec191c30ad9a090ccd96793da63f7
Hubert Olender <olender.hub@gmail.com>
2017-01-24 19:16:26 +0100

    Added component 'news-from-hackernews'

--- frontend/app/components/news-from-hackernews.js
+++ frontend/app/components/news-from-hackernews.js
@@ -1,6 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  sortedAllNews: Ember.computed.sort('allNews', 'sortDefinition'),
+  sortDefinition: ['rank'],
+
   actions: {
     fetchAllNews() {
       this.sendAction('fetchAllNews');
