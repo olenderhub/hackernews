9334c7f2601221fd8d6f8c255195d9f8a61f468f
Hubert Olender <olender.hub@gmail.com>
2017-01-25 02:18:18 +0100

    BE | Fixed saving url

--- backend/app/interactors/fetching_patterns/ycombinator.rb
+++ backend/app/interactors/fetching_patterns/ycombinator.rb
@@ -8,11 +8,12 @@ class FetchingPatterns::Ycombinator
   end
 
   def hash_from_news(news)
-    {
+    hash = {
       news_id: news.attributes['id'].value.to_i,
       rank: news.css('td')[0].text.chop!.to_i,
       title: news.css('td:nth-of-type(3) a').first.text,
-      url: news.css('td:nth-of-type(3) a').first.attr('href')
     }
+    url = news.css('td:nth-of-type(3) a').first.attr('href')
+    hash.merge!(url: url.include?('http') ? url : 'https://news.ycombinator.com/' + url )
   end
 end
